36a591a6147fa1543e454dded4f22e4befe40c5b
Sean McArthur <sean.monstar@gmail.com>
2012-01-27 18:27:11 -0800

    making `shipyard server` be able to server Shipyard files wherever they are on the computer.
    improves the first time use of Shipyard, like if you are following the tutorial

--- scripts/server.js
+++ scripts/server.js
@@ -1,50 +1,75 @@
+/*global __dirname*/
 var http = require('http'),
 	fs = require('fs'),
 	url = require('url'),
 	path = require('path'),
-    object = require('../lib/shipyard/utils/object');
+    object = require('../lib/shipyard/utils/object'),
+	log = require('../lib/shipyard/utils/log');
+
+var HEADERS = { 'Content-Type': 'text/plain' };
+
+function serveFile(res, filename) {
+	fs.readFile(filename, 'binary', function(err, file) {
+		if (err) {
+			log.error("Error for file (%s): %s", filename, err);
+			res.writeHead(500, HEADERS);
+			res.write(err + '\n');
+			res.end();
+			return;
+		}
+		
+		res.writeHead(200);
+		res.write(file, 'binary');
+		res.end();
+
+	});
+}
+
+function e404(res, filename) {
+	log.warn("File not found: %s", filename);
+	res.writeHead(404, HEADERS);
+	res.write('404 Not Found\n');
+	res.end();
+}
+
+function shipyardFile(res, uri) {
+	var root = path.join(__dirname, '../');
+	var syFile = uri.substring(9);
+	var filename = path.join(root, syFile);
+	path.exists(filename, function(exists) {
+		if (exists) {
+			serveFile(res, filename);
+		} else {
+			e404(res, filename);
+		}
+	});
+}
 
 exports.serve = function(dir, port) {
     var ROOT = dir || path.join(__dirname, '../'),
         PORT = port || 8000;
-        HEADERS = { 'Content-Type': 'text/plain' };
 
     var server = http.createServer(function(req, res) {
         var uri = url.parse(req.url).pathname;
-        if (!uri || uri[uri.length-1] == '/') {
+        if (!uri || uri[uri.length-1] === '/') {
             uri = path.join(uri, 'index.html');
         }
 
         var filename = path.join(ROOT, uri);
         path.exists(filename, function(exists) {
             if (exists) {
-                fs.readFile(filename, 'binary', function(err, file) {
-                    if (err) {
-                        console.log("Error for file (%s): %s", filename, err);
-                        res.writeHead(500, HEADERS);
-                        res.write(err + '\n');
-                        res.end();
-                        return
-                    }
-                    
-                    res.writeHead(200);
-                    res.write(file, 'binary');
-                    res.end();
-
-                });
+                serveFile(res, filename);
+			} else if (uri.indexOf('/shipyard') === 0) {
+				shipyardFile(res, uri);
             } else {
-                //404
-                console.log("File not found: %s", filename);
-                res.writeHead(404, HEADERS);
-                res.write('404 Not Found\n');
-                res.end();
+                e404(res, filename);
             }
         });
     });
     server.listen(PORT);
-    console.log('Server running at http://localhost:%d', PORT);
+    log.debug('Server running at http://localhost:%d', PORT);
 };
 
-if (require.main == module) {
+if (require.main === module) {
     exports.serve();
 }
