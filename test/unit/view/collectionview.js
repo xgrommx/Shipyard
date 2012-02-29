d28297bc5fdbcdf30a10a3c296146b65f782a85c
Sean McArthur <sean.monstar@gmail.com>
2012-02-29 12:39:52 -0800

    passing itemViewOptions doesn't remove defaults
    fixes #26

---
+++ test/unit/view/collectionview.js
@@ -0,0 +1,25 @@
+var CollectionView = require('../../../lib/shipyard/view/CollectionView');
+
+module.exports = {
+	
+	'CollectionView': function(it, setup) {
+		it('should know when its empty', function(expect) {
+			var v = new CollectionView();
+			expect(v.isEmpty()).toBe(true);
+
+            v.addItem('test');
+			expect(v.isEmpty()).toBe(false);
+		});
+
+		it('should merge itemViewOptions', function(expect) {
+			var v = new CollectionView({
+				itemViewOptions: {
+					'class': 'example'
+				}
+			});
+
+			expect(v.itemViewOptions.tag).toBe('span');
+		});
+	}
+
+};
