2170a8235bc09665f860edfa0fe8de4a1d5b10c1
Sean McArthur <sean.monstar@gmail.com>
2012-04-25 13:59:09 -0700

    properly handling array.from, adding array.from tests

--- test/unit/utils/array.js
+++ test/unit/utils/array.js
@@ -12,6 +12,40 @@ module.exports = {
 		it('should flatten with `arguments` type', function(expect) {
 			expect(array.flatten([1, 2, arguments])).toBeLike([1, 2, expect]);
 		});
-	}
+	},
+
+    'array.from': function(it, setup) {
+        it('should wrap strings', function(expect) {
+            expect(array.from('abc')).toBeLike(['abc']);
+        });
+
+        it('should wrap numbers', function(expect) {
+            expect(array.from(2)).toBeLike([2]);
+        });
+
+        it('should wrap functions', function(expect) {
+            var fn = function() {};
+            expect(array.from(fn)).toBeLike([fn]);
+        });
+
+        it('should wrap objects', function(expect) {
+            var obj = { foo: 'bar' };
+            expect(array.from(obj)).toBeLike([obj]);
+        });
+
+        it('should not wrap arrays', function(expect) {
+            var a = [];
+            var b = [1, 2, 3];
+
+            expect(array.from(a)).toBe(a);
+            expect(array.from(b)).toBe(b);
+        });
+
+        it('should slice objects that have a length property', function(expect) {
+            // this includes `arguments`
+            var obj = { '0': 'foo', '1': 'bar', 'length': 2 };
+            expect(array.from(obj)).toBeLike(['foo', 'bar']);
+        });
+    }
 
 };
