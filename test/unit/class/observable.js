c77d389ba12d045edd59fde938b8df8d728c8526
Sean McArthur <sean.monstar@gmail.com>
2012-05-21 17:28:24 -0700

    on{Property}Change events now emitted also
    When a property changes, such as "foo", it now emits both propertyChange
    and foodChange event.

--- test/unit/class/observable.js
+++ test/unit/class/observable.js
@@ -31,14 +31,18 @@ module.exports = {
 
         it('should fire a propertyChange event when data changes', function(expect) {
             var o = new Observable(),
-                spy = new Spy();
+                spy = new Spy(),
+                spy2 = new Spy();
             o.addListener('propertyChange', spy);
+            o.addListener('onBadChange', spy2);
+            o.set('sup', 'dude');
             o.set('bad', 'mad');
             o.set('bad', 'happy');
             o.set('bad', 'happy');
 
-            expect(spy.getCallCount()).toBe(2);
+            expect(spy.getCallCount()).toBe(3);
             expect(spy.getLastArgs()).toBeLike(['bad', 'happy', 'mad']);
+            expect(spy2.getCallCount()).toBe(2);
         });
 
         it('should be able to observe properties', function(expect) {
@@ -70,91 +74,91 @@ module.exports = {
             expect(spy.getLastArgs()).toBeLike(['moe', 'baz']);
         });
 
-		it('should be able to set computed properties', function(expect) {
-			var Ex = new Class({
-				Extends: Observable,
-				bar: Observable.property(function(value) {
-					if (arguments.length > 0) {
-						//setter
-						this.set('foo', value);
-					} else {
-						//getter
-						return this.get('foo');
-					}
-				}, 'foo')
-			});
+        it('should be able to set computed properties', function(expect) {
+            var Ex = new Class({
+                Extends: Observable,
+                bar: Observable.property(function(value) {
+                    if (arguments.length > 0) {
+                        //setter
+                        this.set('foo', value);
+                    } else {
+                        //getter
+                        return this.get('foo');
+                    }
+                }, 'foo')
+            });
 
-			var ex = new Ex({ foo: 'baz' });
-			var fooSpy = new Spy();
-			var barSpy = new Spy();
+            var ex = new Ex({ foo: 'baz' });
+            var fooSpy = new Spy();
+            var barSpy = new Spy();
 
-			ex.observe('foo', fooSpy);
-			ex.observe('bar', barSpy);
-			ex.set('bar', 'derp');
+            ex.observe('foo', fooSpy);
+            ex.observe('bar', barSpy);
+            ex.set('bar', 'derp');
 
-			expect(ex.get('foo')).toBe('derp');
-			expect(fooSpy.getCallCount()).toBe(1);
-			expect(barSpy.getCallCount()).toBeTruthy();
-		});
+            expect(ex.get('foo')).toBe('derp');
+            expect(fooSpy.getCallCount()).toBe(1);
+            expect(barSpy.getCallCount()).toBeTruthy();
+        });
 
         it('should assign events for properties starting with "on"', function(expect) {
             var spy = new Spy();
-			var spy2 = new Spy();
+            var spy2 = new Spy();
             var Ex = new Class({
                 Extends: Observable,
                 onEv: spy
             });
             var ex = new Ex({
-				onFoo: spy2
-			});
+                onFoo: spy2
+            });
             ex.emit('ev');
-			ex.emit('foo');
+            ex.emit('foo');
 
             expect(spy).toHaveBeenCalled();
-			expect(spy2).toHaveBeenCalled();
+            expect(spy2).toHaveBeenCalled();
         });
 
-		it('should deep observe properties', function(expect) {
-			var a = new Observable();
-			var b = new Observable({ a: a });
-			var spy = new Spy();
-			b.observe('a', spy);
+        it('should deep observe properties', function(expect) {
+            var a = new Observable();
+            var b = new Observable({ a: a });
+            var spy = new Spy();
+            b.observe('a', spy);
 
-			a.set('foo', 'bar');
-			expect(spy).toHaveBeenCalled();
+            a.set('foo', 'bar');
+            expect(spy).toHaveBeenCalled();
 
-			var c = new Observable();
-			var spy2 = new Spy();
-			b.observe('a', spy2);
-			b.set('a', c);
-			expect(spy2).toHaveBeenCalled();
+            var c = new Observable();
+            var spy2 = new Spy();
+            b.observe('a', spy2);
+            b.set('a', c);
+            expect(spy2).toHaveBeenCalled();
 
-			var spy3 = new Spy();
-			b.observe('a', spy3);
+            var spy3 = new Spy();
+            b.observe('a', spy3);
 
-			// a is no longer a prop of b, so it shouldn't care anymore
-			a.set('herp', 'derp');
-			expect(spy3).not.toHaveBeenCalled();
-		});
+            // a is no longer a prop of b, so it shouldn't care anymore
+            a.set('herp', 'derp');
+            expect(spy3).not.toHaveBeenCalled();
+        });
 
-		it('should be able to not-deep observe', function(expect) {
-			var a = new Observable();
-			var b = new Observable({ a: a });
-			var spy = this.createSpy();
-			b.observe('a', spy, false);
+        it('should be able to not-deep observe', function(expect) {
+            var a = new Observable();
+            var b = new Observable({ a: a });
+            var spy = this.createSpy();
+            b.observe('a', spy, false);
 
-			a.set('c', 'd');
-			expect(spy).not.toHaveBeenCalled();
+            a.set('c', 'd');
+            expect(spy).not.toHaveBeenCalled();
 
-			var spy2 = this.createSpy();
-			b.observe('a', spy2);
-			a.set('f', 'g');
-			expect(spy2).toHaveBeenCalled();
-			expect(spy).not.toHaveBeenCalled();
+            var spy2 = this.createSpy();
+            b.observe('a', spy2);
+            a.set('f', 'g');
+            expect(spy2).toHaveBeenCalled();
+            expect(spy).not.toHaveBeenCalled();
 
-			var e = new Observable();
-			b.set('a', e);
-			expect(spy).toHaveBeenCalled();
-		});
+            var e = new Observable();
+            b.set('a', e);
+            expect(spy).toHaveBeenCalled();
+        });
     }
 };
